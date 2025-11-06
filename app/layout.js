62906f6b5556164fbaa44330231a3aa42596a231
Pranav Reddy <77679001+PranavReddyy@users.noreply.github.com>
2025-11-06 12:14:34 +0530

    Adding Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth-context";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
           backgroundColor: "#000000",
         }}
       >
+        <Analytics />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
